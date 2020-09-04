f4cdb7b98b3130578ff0dd041abc1e04c513a28d
delims <delims@qq.com>
2020-09-04 15:50:22 +0800

    Initial Commit

---
+++ LeetCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LeetCode
+//
+//  Created by delims on 2020/9/4.
+//  Copyright © 2020 delims. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
